f48031f72b33cc0ee366df6e47bf2eff348dfb0b
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-06-22 23:41:53 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OldFilesCleaner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
