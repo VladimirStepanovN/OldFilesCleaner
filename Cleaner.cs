8b5e341b33e34952b6fe2f2f29e86255fff28e92
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-12-04 02:53:02 +0500

    Задание 1 (HW-03).

---
+++ Cleaner.cs
@@ -0,0 +1,54 @@
+﻿namespace OldFilesCleaner
+{
+    public class Cleaner
+    {
+        public void Clean(string Path)
+        {
+            try
+            {
+                var directory = new DirectoryInfo(Path);
+                if (directory.Exists)
+                {
+                    foreach (DirectoryInfo dir in directory.GetDirectories())
+                    {
+                        try
+                        {
+                            Clean(dir.FullName);
+                        }
+                        catch (UnauthorizedAccessException)
+                        {
+                            Console.WriteLine("Недопустимое действие или недостаточно прав на выполнение.");
+                            continue;
+                        }
+                    }
+                    FileInfo[] files = directory.GetFiles();
+                    if (files.Length > 0)
+                    {
+                        foreach (FileInfo file in files)
+                        {
+                            try
+                            {
+                                if (file.LastAccessTime.AddMinutes(30.0) < DateTime.Now)
+                                    file.Delete();
+                            }
+                            catch (UnauthorizedAccessException)
+                            {
+                                Console.WriteLine("Недопустимое действие или недостаточно прав на выполнение.");
+                                continue;
+                            }
+                        }
+                    }
+                    if (directory.GetFiles().Length == 0 && directory.GetDirectories().Length == 0) directory.Delete();
+                }
+                else
+                {
+                    Console.WriteLine("Указанный каталог не существует");
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите путь к каталогу:");
+            string? Path = Console.ReadLine();
+            if (!string.IsNullOrEmpty(Path) && !string.IsNullOrWhiteSpace(Path))
+            {
+                try
+                {
+                    Cleaner cleaner = new();
+                    cleaner.Clean(Path);
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    Console.WriteLine("Недопустимая часть пути к каталогу.");
+                }
+                catch (PathTooLongException)
+                {
+                    Console.WriteLine("Путь или имя файла превышает максимальную длину, определенную системой.");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Путь к каталогу не указан.");
+            }
         }
     }
 }
